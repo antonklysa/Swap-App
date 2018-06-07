b0c6909fd3825466566bf167b81c197c70dadc84
Anton Klysa <aklysa@rubiconware.com>
2018-06-07 12:24:49 +0300

    project was created

---
+++ Swap App/Swap App/Controllers/Network/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Swap App
+//
+//  Created by Anton Klysa on 6/7/18.
+//  Copyright © 2018 Anton Klysa. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: NSObject {
+    <#code#>
+}
---
+++ Swap App/Swap App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swap App
+//
+//  Created by Anton Klysa on 6/7/18.
+//  Copyright © 2018 Anton Klysa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
